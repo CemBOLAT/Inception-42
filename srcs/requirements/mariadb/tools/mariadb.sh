7642cc91482feec3314e58f9caa2cbb23f820fba
CemBOLAT <cemalbolat200@outlook.com>
2024-02-15 22:59:02 +0300

    5

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -5,8 +5,8 @@ service mariadb start
 sleep 2
 
 mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
+mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
 mariadb -e "FLUSH PRIVILEGES;"
 mariadb -e "SHUTDOWN;"
 
