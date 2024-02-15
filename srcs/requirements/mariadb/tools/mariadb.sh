1182bdef9476c5e31069282ded69f8577cf7b0aa
Cemal BOLAT <103999323+CemBOLAT@users.noreply.github.com>
2024-02-15 11:46:55 +0300

    Update mariadb.sh

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -10,6 +10,8 @@ mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
 mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
-#mysql -e "quit;"
+mysql -e "SHUTDOWN;"
+
+exec "$@"
 
 
