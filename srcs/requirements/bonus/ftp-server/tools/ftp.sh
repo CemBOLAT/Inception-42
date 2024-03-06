19e9f4e290ed86cb90e2f9274c69993ffe4e38af
Cemal BOLAT <103999323+CemBOLAT@users.noreply.github.com>
2024-03-07 02:06:26 +0300

    redis

--- srcs/requirements/bonus/ftp-server/tools/ftp.sh
+++ srcs/requirements/bonus/ftp-server/tools/ftp.sh
@@ -2,6 +2,6 @@
 
 # This script is used to start the ftp server
 
+echo $DB_USER | tee  -a /etc/vsftpd/vsftpd.userlist
 # Start the ftp server
-vsftpd /etc/vsftpd/vsftpd.conf
-
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
