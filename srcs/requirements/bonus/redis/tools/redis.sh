0a3589dac0937e5aa2b83ad1759690ba32dc8686
Cemal BOLAT <103999323+CemBOLAT@users.noreply.github.com>
2024-03-07 01:52:40 +0300

    redis

--- srcs/requirements/bonus/redis/tools/redis.sh
+++ srcs/requirements/bonus/redis/tools/redis.sh
@@ -5,4 +5,8 @@ sed -i 's/bind 127.0.0.1/#bind 127.0.0.1/' /etc/redis/redis.conf
 sed -i "s|# maxmemory <bytes>|maxmemory 2mb|g" /etc/redis/redis.conf
 sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" /etc/redis/redis.conf
 
+echo 'vm overcommit_memory=1' >> /etc/sysctl.conf
+echo 'madvise never' >> /sys/kernel/mm/transparent_hugepage/enabled
+echo 'madvise never' >> /etc/rc.local
+
 redis-server --protected-mode no
