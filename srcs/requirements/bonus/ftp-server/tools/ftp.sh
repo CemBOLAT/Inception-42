46bc39b13841d82f1a023d27454c9460892d69c8
Cemal BOLAT <103999323+CemBOLAT@users.noreply.github.com>
2024-03-07 13:34:17 +0300

    ftp

--- srcs/requirements/bonus/ftp-server/tools/ftp.sh
+++ srcs/requirements/bonus/ftp-server/tools/ftp.sh
@@ -8,7 +8,6 @@ if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
     mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
 
     # Add the FTP_USER, change his password and declare him as the owner of wordpress folder and all subfolders
-    adduser $FTP_USR --disabled-password
     echo "$FTP_USR:$FTP_PWD" | /usr/sbin/chpasswd &> /dev/null
     chown -R $FTP_USR:$FTP_USR /var/www/html
     echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
