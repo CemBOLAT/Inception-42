9de05abde7540377ef8ee79e9bc810100f7c0515
Cemal BOLAT <103999323+CemBOLAT@users.noreply.github.com>
2024-03-07 13:48:47 +0300

    ftp

--- srcs/requirements/bonus/ftp-server/tools/ftp.sh
+++ srcs/requirements/bonus/ftp-server/tools/ftp.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
+# Create the directory for WordPress
+mkdir -p /var/www/html
 
-    mkdir -p /var/www/html
-
-    cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.bak
-    mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
-
-    # Add the FTP_USER, change his password and declare him as the owner of wordpress folder and all subfolders
-    echo "$FTP_USR:$FTP_PWD" | /usr/sbin/chpasswd &> /dev/null
-    chown -R $FTP_USR:$FTP_USR /var/www/html
-    echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
-
-fi
+# Add the FTP_USER, change his password, and declare him as the owner of the WordPress folder and all subfolders
+adduser --disabled-password --gecos "" $FTP_USR
+chown -R $FTP_USR:$FTP_USR /var/www/html
+echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
 
 echo "FTP started on :21"
-vsftpd /etc/vsftpd/vsftpd.conf
+vsftpd /etc/vsftpd.conf
