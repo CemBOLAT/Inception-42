d72a4e20272f05755ec6faf13b759cd848e26495
Cemal BOLAT <103999323+CemBOLAT@users.noreply.github.com>
2024-03-07 13:57:10 +0300

    ftp

--- srcs/requirements/bonus/ftp-server/tools/ftp.sh
+++ srcs/requirements/bonus/ftp-server/tools/ftp.sh
@@ -3,6 +3,9 @@
 # Create the directory for WordPress
 mkdir -p /var/www/html
 
+# Print out the value of FTP_USR for debugging
+echo "FTP_USR is set to: $FTP_USR"
+
 # Add the FTP_USER, change his password, and declare him as the owner of the WordPress folder and all subfolders
 adduser --disabled-password --gecos "" $FTP_USR
 chown -R $FTP_USR:$FTP_USR /var/www/html
