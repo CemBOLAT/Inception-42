b0fbf11076061cc5343f1b69a4148f3d634f9718
CemBOLAT <cemalbolat2003@outlook.com>
2024-03-08 13:35:34 +0300

    port missmatch

--- srcs/requirements/bonus/static_site/tools/app.js
+++ srcs/requirements/bonus/static_site/tools/app.js
@@ -5,6 +5,6 @@ const app = express()
 app.use(express.static('public'))
 
 
-app.listen(3000, () => {
-    console.log("server running on port 3000")
+app.listen(3030, () => {
+    console.log("server running on port 3030")
 })
