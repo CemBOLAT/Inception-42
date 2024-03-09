48809e69031d19901f461fa28a99231dd86c0826
CemBOLAT <cemalbolat2003@outlook.com>
2024-03-10 02:15:20 +0300

    finish

--- srcs/requirements/bonus/ftp-server/tools/ftp-server.sh
+++ srcs/requirements/bonus/ftp-server/tools/ftp-server.sh
@@ -3,6 +3,10 @@
 # Add the USER if it doesn't exist
 if ! id "$FTP_USER" &>/dev/null; then
     adduser --disabled-password --gecos "" $FTP_USER
+    # Add user to a password file
+    echo "$FTP_USER:$FTP_PASS" | chpasswd -c SHA256
+    # Add user to a list of users
+
     echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
 fi
 
@@ -17,5 +21,7 @@ if [ ! -d "/home/$FTP_USER/ftp/files" ]; then
     mkdir /home/$FTP_USER/ftp/files
     chown $FTP_USER:$FTP_USER /home/$FTP_USER/ftp/files
 fi
+
+mkdir -p /var/run/vsftpd/empty
 
 exec "$@"
